9d78427856984e59e1abc8b89bb104471416097b
joseph farrell <224jfarrell@frhsd.com>
2023-11-08 10:43:28 -0500

    font update 2 billion

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 const fonts = ['Agbalumo', 'Bebas Neue', 'Edu TAS Beginner', 'Graduate', 'IBM Plex Sans', 'Josefin Slab', 'Linefont', 'Monofur', 'Montserrat', 'Nabla', 'Noto Sans Japanese', 'Noto Sans Korean', 'Noto Serif Simplified Chinese', 'Nunito Sans', 'Open Sans', 'Roboto Mono'];
 let fontResult;
 let i = 0;
+let j = 0;
+let k = 0;
 let A = document.getElementById('header');
+let B = document.getElementById('prog');
+let C = document.getElementById('tooltips');
+let D = document.getElementById('percent');
+
 function fontOrder(font){
     if(i < fonts.length){
         i++;
         font = fonts[i - 1];
-        console.log(font);
     }
     if(i == fonts.length){
         setTimeout(i = 0, 500);
@@ -72,3 +77,90 @@ function fontOrder(font){
     }
 }
 setInterval(fontOrder, 100);
+
+function fontLoad(font){
+    if(k < fonts.length){
+        k++;
+        font = fonts[k - 1];
+    }
+    if(k == fonts.length){
+        font = "Segoe UI";
+    }
+    switch(font){
+        case 'Agbalumo':
+            C.style.fontFamily = "'Agbalumo', script";
+            break;
+        case 'Bebas Neue':
+            C.style.fontFamily = "'Bebas Neue', sans-serif";
+            break;
+        case 'Edu TAS Beginner':
+            C.style.fontFamily = "'Edu TAS Beginner', sans-serif";
+            break;
+        case 'Graduate':
+            C.style.fontFamily = "'Graduate', serif";
+            break;
+        case 'IBM Plex Sans':
+            C.style.fontFamily = "'IBM Plex Sans', sans-serif";
+            break;
+        case 'Josefin Slab':
+            C.style.fontFamily = "'Josefin Slab', serif";
+            break;
+        case 'Linefont':
+            C.style.fontFamily = "'Linefont', script";
+            break;
+        case 'Monofur':
+            C.style.fontFamily = "'Monofur', monospace";
+            break;
+        case 'Montserrat':
+            C.style.fontFamily = "'Montserrat', sans-serif";
+            break;
+        case 'Nabla':
+            C.style.fontFamily = "'Nabla', script";
+            break;
+        case 'Noto Sans Japanese':
+            C.style.fontFamily = "'Noto Sans JP', sans-serif";
+            C.innerText = "フォントを読み込んでいます...";
+            break;
+        case 'Noto Sans Korean':
+            C.style.fontFamily = "'Noto Sans KR', sans-serif";
+            C.innerText = "글꼴 로드 중...";
+            break;
+        case 'Noto Serif Simplified Chinese':
+            C.style.fontFamily = "'Noto Serif SC', serif";
+            C.innerText = "正在加载字体...";
+            break;
+        case 'Nunito Sans':
+            C.style.fontFamily = "'Nunito Sans', sans-serif";
+            C.innerText = "Loading fonts...";
+            break;
+        case 'Open Sans':
+            C.style.fontFamily = "'Open Sans', sans-serif";
+            break;
+        case 'Roboto Mono':
+            C.style.fontFamily = "'Roboto Mono', monospace";
+            C.innerText = "Завантаження шрифтів..."
+            break;
+        case 'Segoe UI':
+            C.style.fontFamily = "'Segoe UI', sans-serif";
+            C.innerText = 'Fonts loaded!';
+        default:
+            console.log('nothing');
+            break;
+    }
+}
+
+function load(){
+    if(j < 1000){
+        j++;
+        B.style.width = `${j / 10}%`
+        D.innerText = `${j / 10}%`
+    }
+    if(j == 100){
+        C.innerText = "Loading fonts..."
+        setInterval(fontLoad, 350)
+    }
+    if(j == 1000){
+        k = 210;
+    }
+}
+setInterval(load, 20);
